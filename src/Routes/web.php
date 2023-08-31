b1a190a0b73b72715ef3736177e6e702764990e0
Jewel Rana <jewelrana.dev@gmail.com>
2023-08-31 15:31:57 +0600

    Restricted with permission or role done

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Rajtika\Mongovity\Constants\Mongovity;
 use Rajtika\Mongovity\Http\Controllers\MongoActivityController;
 
-Route::group(['prefix' => 'mongovity', 'middleware' => ['session','auth', 'web']], function() {
+Route::group(['prefix' => 'mongovity', 'middleware' => ['web', 'session_bind_user_agent']], function() {
     Route::get('/', [MongoActivityController::class, 'index'])
         ->middleware('role_or_permission:admin|activity_logs')
         ->name(Mongovity::NAMESPACE);

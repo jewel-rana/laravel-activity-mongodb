d993582fa3b19b8688dcae2114178bff3a96e200
Jewel Rana <jewel@newroztech.com>
2026-06-11 15:28:57 +0600

    Fix PHP 8.4 nullable parameter deprecation in activity() helper

--- src/Helper/Activity.php
+++ src/Helper/Activity.php
@@ -3,7 +3,7 @@
 use Rajtika\Mongovity\Services\Mongovity;
 
 if (! function_exists('activity')) {
-    function activity(string $logName = null): Mongovity
+    function activity(?string $logName = null): Mongovity
     {
         return app(Mongovity::class);
     }
